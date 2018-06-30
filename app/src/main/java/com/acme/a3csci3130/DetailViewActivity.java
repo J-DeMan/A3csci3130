c284116e7fb337b9388e38e6d9c609de8c577d22
Jerrett DeMan <jerrett.deman@gmail.com>
2018-06-30 18:43:39 -0300

    Adds create business activity

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -3,26 +3,31 @@ package com.acme.a3csci3130;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
-    Contact receivedPersonInfo;
+    private EditText numberField, nameField, primaryBusinessField, addressField, provinceFeild;
+    Business receivedPersonInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
-        receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
+        receivedPersonInfo = (Business)getIntent().getSerializableExtra("Business");
 
+        numberField = (EditText) findViewById(R.id.number);
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        primaryBusinessField = (EditText) findViewById(R.id.primaryBusiness);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceFeild = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
+            numberField.setText(receivedPersonInfo.number);
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            primaryBusinessField.setText(receivedPersonInfo.primaryBusiness);
+            addressField.setText(receivedPersonInfo.address);
+            provinceFeild.setText(receivedPersonInfo.province);
         }
     }
 
